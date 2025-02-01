e5d1a8b0f5a7198472d6a3b3a4a0557f7e2d34c9
arkochan <najmussakibarko@gmail.com>
2025-02-01 17:48:39 +0600

    placeholders added

---
+++ src/app/media/[m_id]/page.tsx
@@ -0,0 +1,13 @@
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ m_id: string }>
+}) {
+  const slug = (await params).m_id
+  return (
+    < div className="text-white" >
+      <h1>Media Page</h1>
+      <p>Media ID: {slug}</p>
+    </div >
+  )
+}
---
+++ src/app/player/[m_id]/page.tsx
@@ -0,0 +1,12 @@
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ m_id: string }>
+}) {
+  const slug = (await params).m_id
+  return (
+    < div className="text-white" >
+      <h1>Player Page</h1>
+      <p>Media ID: {slug}</p>
+    </div >)
+}
