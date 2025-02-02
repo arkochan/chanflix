52fb2504e56933ca4f9e398771b2104a7c340694
arkochan <najmussakibarko@gmail.com>
2025-02-02 07:43:30 +0600

    uses new details

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -2,14 +2,16 @@ import movieDetail from "@/lib/MovieDetail.json";
 import searchResult from "@/lib/MultiResult.json";
 import seasonDetail from "@/lib/TVSeasonDetail.json";
 import seriesDetail from "@/lib/TVSeriesDetail.json";
+import { MovieDetail } from "@/types/MovieDetail";
 import { SearchResult } from "@/types/ResultMetaData";
+import { TVDetail } from "@/types/TVDetail";
 
-export function getMovieDetail(id: number) {
-  return movieDetail;
+export function getMovieDetail(id: number): MovieDetail {
+  return movieDetail as MovieDetail;
 }
 
-export function getTvDetail(id: number) {
-  return seriesDetail;
+export function getTvDetail(id: number): TVDetail {
+  return seriesDetail as TVDetail;
 }
 
 export function getSearchResult(id: number): SearchResult {
