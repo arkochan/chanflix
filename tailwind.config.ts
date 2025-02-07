1b93903c4be99b65fa6e188c93cd53c0dabc2931
arkochan <najmussakibarko@gmail.com>
2025-02-07 21:01:28 +0600

    flowbite carsl

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,11 @@
 import type { Config } from "tailwindcss";
-
+const flowbite = require("flowbite-react/tailwind");
 export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    flowbite.content(),
   ],
   theme: {
     extend: {
@@ -16,5 +17,6 @@ export default {
   },
   plugins: [
     require('@tailwindcss/line-clamp'),
+    flowbite.plugin(),
   ],
 } satisfies Config;
