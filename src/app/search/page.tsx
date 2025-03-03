21ca0ded65e274c8170f941a52c097535808f7fd
arkochan <najmussakibarko@gmail.com>
2025-03-03 21:56:10 +0600

    popular show titiles dynamic

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,10 +1,13 @@
 import HeroSearch from "@/components/HeroSearch";
 import SearchResults from "@/components/SearchResults";
+import { getTopMovies } from "@/lib/api/movies";
 
-export default function Home() {
+export default async function Home() {
+  let topMovies = await getTopMovies();
+  const popular_show_titles = topMovies.map((movie) => movie.title || movie.name || "");
   return (
-    <div className="m-10 flex flex-col  bg-black text-white font-extrabold flex h-dvh text-4xl" style={{}}>
-      <HeroSearch className="mt-10 max-w-[700]" />
-      <SearchResults SearchResults={["SANIC"]} />
+    <div className="m-10 flex-col  bg-black text-white font-extrabold flex h-dvh text-4xl" style={{}}>
+      <HeroSearch topMovies={popular_show_titles} className="mt-10 max-w-[700]" />
+      <SearchResults />
     </div>);
 }
