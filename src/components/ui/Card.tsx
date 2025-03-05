d04326387dc7798ffda5d0fa42e559baff604879
arkochan <najmussakibarko@gmail.com>
2025-03-05 21:11:58 +0600

    cardsize fixed

--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -7,16 +7,19 @@ interface CardProps {
 }
 function Card({ media }: CardProps) {
   const posterBaseUrl = "https://image.tmdb.org/t/p/w600_and_h900_bestv2"
+  const mediaType = media.media_type == undefined ? "movie" : media.media_type;
   return (
-    <Link href={`/detail/${media.media_type}/${media.id}`} className="max-w-xs rounded overflow-hidden shadow-lg">
-      <img className="w-full" src={posterBaseUrl + media.poster_path} alt={media.title} />
-      <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2">{media.title}{media.name}</div>
-        <p className="text-gray-700 text-base line-clamp-3 overflow-hidden">
-          {media.overview}
-        </p>
-      </div>
-    </Link>
+    <div className='max-w-xs rounded overflow-hidden shadow-lg'>
+      <Link href={`/detail/${mediaType}/${media.id}`} className="">
+        <img className="w-full" src={posterBaseUrl + media.poster_path} alt={media.title} />
+        <div className="px-6 py-4">
+          <div className="font-bold text-xl mb-2">{media.title}{media.name}</div>
+          <p className="text-gray-700 text-base line-clamp-3 overflow-hidden">
+            {media.overview}
+          </p>
+        </div>
+      </Link>
+    </div>
   );
 };
 
